c6cc97610f5e891bc3d94ce558d99846f2c74c12
kantimam <kantemir.imam@gmail.com>
2020-09-09 18:16:39 +0200

    user model reworked

--- models/user/user.go
+++ models/user/user.go
@@ -11,14 +11,19 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name" gorm:"unique" form:"name"`
 	Password string `json:"password" form:"password"`
-	Type     int    `json:"type" gorm:"default:5"  form:"type"`
 }
 
-// Preview is a simpler version of Post used to display a preview in a list
-type Preview struct {
-	ID           uint   `json:"id"`
-	Title        string `json:"title"`
-	ResourceName string `json:"resourceName"`
+// lets not deal with admins and moderators for now :)
+/* type User struct {
+	gorm.Model
+	Name     string `json:"name" gorm:"unique" form:"name"`
+	Password string `json:"password" form:"password"`
+	Type     int    `json:"type" gorm:"default:5"  form:"type"`
+}
+*/
+type BasicUser struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
 }
 
 // GetUsers returns all posts from database
@@ -27,6 +32,26 @@ func GetUsers() []User {
 	var user []User
 	database.DBConn.Model(&User{}).Find(&user)
 	return user
+}
+
+// GetBasicUserByID finds a user by its ID and returns a simple user object without password obiously (should be use to get user data)
+func GetBasicUserByID(id uint) (User, error) {
+	var user User
+	response := database.DBConn.Model(&User{}).First(id)
+	if response.Error != nil {
+		return user, response.Error
+	}
+	return user, nil
+}
+
+// GetUserByID finds a user by its ID and returns the entire user data (should only be used to check login)
+func GetUserByID(id uint) (BasicUser, error) {
+	var user BasicUser
+	response := database.DBConn.Model(&User{}).First(id)
+	if response.Error != nil {
+		return user, response.Error
+	}
+	return user, nil
 }
 
 // CreateUser does what it says :3
